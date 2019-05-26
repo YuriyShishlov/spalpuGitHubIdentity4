f7cf5b7df7287a3226dbe62047edb5de40d8f4c4
Yuriy <georgheim@mail.ru>
2019-05-26 21:12:23 +0600

    Улучшил для мобильных. Добавил статьи, иконку

--- spalpuGitHubIdentity4/Controllers/SpaController.cs
+++ spalpuGitHubIdentity4/Controllers/SpaController.cs
@@ -8,23 +8,39 @@ namespace spalpuGitHubIdentity4.Controllers
 {
     public class spaController : Controller
     {
-        public ActionResult Spa()
+        public ActionResult spa()
         {
             return View();
         }
-        public ActionResult Aussage()
+        public ActionResult aussage()
         {
             return View();
         }
-        public ActionResult GlobalIssues()
+        public ActionResult globalIssues()
         {
             return View();
         }
-        public ActionResult More_about_health()
+        public ActionResult more_about_health()
         {
             return View();
         }
-        public ActionResult Spastic_muscle_condition()
+        public ActionResult spastic_muscle_condition()
+        {
+            return View();
+        }
+        public ActionResult balanced_diet()
+        {
+            return View();
+        }
+        public ActionResult physical_activity()
+        {
+            return View();
+        }
+        public ActionResult health_education()
+        {
+            return View();
+        }
+        public ActionResult biomechanics()
         {
             return View();
         }
